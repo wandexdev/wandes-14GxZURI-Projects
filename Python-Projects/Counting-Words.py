ac708e4b0dcd284fbf68c00bffddd5cbede9a417
wandexdev <tiwande04@gmail.com>
2022-05-23 05:28:23 +0100

    first python files

---
+++ Python-Projects/Counting-Words.py
@@ -0,0 +1,7 @@
+file = open("myfile.txt", "r")
+count = 0
+for line in file:
+        words = line.split()
+        count += len(words)
+file.close()
+print("Number of words in a Text file: ", count)
